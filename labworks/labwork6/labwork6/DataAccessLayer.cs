99dc06db87650c70a30fad96a46fe60e231a6c7f
DeDomosed <poperm137@gmail.com>
2025-09-29 12:58:05 +0300

    lb7

---
+++ labworks/labwork6/labwork6/DataAccessLayer.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace labwork6
+{
+    public static class DataAccessLayer
+    {
+        private static string _server = "mssql";
+        private static string _datebase = "ispp3101";
+        private static string _userLogin = "ispp3101";
+        private static string _password = "3101";
+
+        public static string ConnectionString
+        {
+            get
+            {
+                SqlConnectionStringBuilder _builder = new()
+                {
+                    DataSource = _server,
+                    InitialCatalog = _datebase,
+                    UserID = _userLogin,
+                    Password = _password,
+                    TrustServerCertificate = true
+                };
+
+                return _builder.ConnectionString;
+            }
+        }
+
+        public static void ChangeConnectionSettings(string server, string database, string login, string password)
+        {
+            _server = server;
+            _datebase = database;
+            _userLogin = login;
+            _password = password;
+        }
+
+        public static bool TryConnection()
+        {
+            using SqlConnection connection = new(ConnectionString);
+
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (SqlException ex) 
+            {
+                return false;
+            }
+        }
+
+        public static async Task<int> UpdateRowsCommandAsync(string sqlCommand)
+        {
+            await using SqlConnection connection = new(ConnectionString);
+
+            try
+            {
+                await connection.OpenAsync();
+                SqlCommand command = new SqlCommand(sqlCommand, connection);
+                int changedRowsAmount = await command.ExecuteNonQueryAsync();
+                return changedRowsAmount;
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return -1;
+            }
+        }
+
+        public static async Task<object?> SelectRowsCommandAsync(string sqlCommand)
+        {
+            await using SqlConnection connection = new(ConnectionString);
+
+            try
+            {
+                await connection.OpenAsync();
+                SqlCommand command = new SqlCommand(sqlCommand, connection);
+
+                return await command.ExecuteScalarAsync();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+
+        }
+
+        public static async Task ChangeSessionPriceAsync(decimal newPrice, int sessionId)
+        {
+            await using SqlConnection connection = new(ConnectionString);
+
+            try
+            {
+                await connection.OpenAsync();
+
+                string query = "UPDATE Session SET Price = @newPrice WHERE SessionId = @sessionId";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@newPrice", newPrice);
+                command.Parameters.AddWithValue("@sessionId", sessionId);
+
+                await command.ExecuteNonQueryAsync();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+        public static async Task UploadFile(int filmId, string fileName)
+        {
+            await using SqlConnection connection = new(ConnectionString);
+
+            try
+            {
+                await connection.OpenAsync();
+
+                string query = "UPDATE Film SET Poster = @fileName WHERE filmId = @filmId";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@filmId", filmId);
+                command.Parameters.AddWithValue("@fileName", fileName);
+
+                await command.ExecuteNonQueryAsync();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ labworks/labwork6/labwork6/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace labwork6
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine(DataAccessLayer.ConnectionString);
+
+            //DataAccessLayer.ChangeConnectionSettings("pgsad", "ispp0101", "user", "12134");
+            //Console.WriteLine(DataAccessLayer.ConnectionString);
+            //DataAccessLayer.TryConnection();
+
+            DataAccessLayer.ChangeConnectionSettings("mssql", "ispp3101", "ispp3101", "3101");
+            Console.WriteLine(DataAccessLayer.ConnectionString);
+
+            if (DataAccessLayer.TryConnection())
+                Console.WriteLine("Подключение успешно");
+
+            //Task 2
+            Console.WriteLine($"Изменено строк: {await DataAccessLayer.UpdateRowsCommandAsync("UPDATE Game SET Price=1100 WHERE GameId = 11")}");
+            Console.WriteLine(await DataAccessLayer.SelectRowsCommandAsync("SELECT * FROM Game WHERE GameId = 10"));
+
+            //Task 1
+            DataAccessLayer.ChangeSessionPriceAsync(350, 3);
+
+            byte[] fileData = File.ReadAllBytes(Path.Combine(Environment.CurrentDirectory, "")); // чтение содержимого файла
+            if (fileData.Length < 1024) // проверка, что размер меньше 1 КБ
+            {
+                // выполнение SqlCommand с параметром, в параметр передать значение fileData
+            }
+        }
+    }
+}
