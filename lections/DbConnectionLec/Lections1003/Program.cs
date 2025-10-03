bac7e2d3f99b906f05ebaa02765ba664bf196a3c
DeDomosed <poperm137@gmail.com>
2025-10-03 09:42:53 +0300

    lec0310

---
+++ lections/DbConnectionLec/Lections1003/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+Console.WriteLine("Разработка клиента");
+
+var connectionString = "...";
+using IDbConnection connection = new SqlConnection(connectionString);
+string query = "";
+connection.Open();
+
