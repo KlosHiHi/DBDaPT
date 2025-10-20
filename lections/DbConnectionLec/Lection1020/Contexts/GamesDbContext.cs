4d5a4b06441ce9cf532695fa338a5ad46016dd3b
DeDomosed <poperm137@gmail.com>
2025-10-20 15:16:07 +0300

    lec1020

---
+++ lections/DbConnectionLec/Lection1020/Contexts/GamesDbContext.cs
@@ -0,0 +1,55 @@
+﻿using Lection1020.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lection1020.Contexts;
+
+public partial class GamesDbContext : DbContext
+{
+    public GamesDbContext()
+    {
+    }
+
+    public GamesDbContext(DbContextOptions<GamesDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Game> Games { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Data Source=mssql;Initial Catalog=ispp3101;User ID=ispp3101;Password=3101;Trust Server Certificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PK__Category__19093A0B2F2D3DAE");
+
+            entity.ToTable("Category");
+
+            entity.Property(e => e.Name).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Game>(entity =>
+        {
+            entity.HasKey(e => e.GameId).HasName("PK__Game__2AB897FDBE6BE58C");
+
+            entity.ToTable("Game");
+
+            entity.Property(e => e.Description).HasMaxLength(500);
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Price).HasColumnType("decimal(16, 2)");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Games)
+                .HasForeignKey(d => d.CategoryId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Game_Category");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ lections/DbConnectionLec/Lection1020/Program.cs
@@ -0,0 +1,94 @@
+﻿using Lection1020.Contexts;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Выполнение SQL-запросов средсвами ORM");
+
+using GamesDbContext context = new();
+
+var games = context.Games
+    .Where(g => EF.Functions.Like(g.Name, "[a-d]%"));
+Console.WriteLine(games.ToQueryString());
+
+static async Task FromSql(GamesDbContext context)
+{
+    var games = context.Games
+        .FromSql($"select * from game");
+    Console.WriteLine(games.ToQueryString());
+    await games.ForEachAsync(g =>
+        Console.WriteLine($"{g.Name}, {g.Price} руб. [{g.CategoryId}]"));
+
+    Console.WriteLine();
+
+    int price = 1000;
+    games = context.Games
+        .FromSql($"select * from game where price < {price}");
+    Console.WriteLine(games.ToQueryString());
+    await games.ForEachAsync(g =>
+        Console.WriteLine($"{g.Name}, {g.Price} руб. [{g.CategoryId}]"));
+
+    Console.WriteLine();
+
+    string columnName = "price";
+    games = context.Games
+        .FromSqlRaw($"select * from game order by {columnName}");
+    Console.WriteLine(games.ToQueryString());
+    await games.ForEachAsync(g =>
+        Console.WriteLine($"{g.Name}, {g.Price} руб. [{g.CategoryId}]"));
+
+    Console.WriteLine();
+
+    string title = "SimCity";
+    games = context.Games
+        .FromSqlRaw($"select * from game where name = '{title}'");
+    Console.WriteLine(games.ToQueryString());
+    await games.ForEachAsync(g =>
+        Console.WriteLine($"{g.Name}, {g.Price} руб. [{g.CategoryId}]"));
+
+    Console.WriteLine();
+
+    title = "SimCity";
+    games = context.Games
+        .FromSqlRaw($"select * from game where name = @p0", title);
+    Console.WriteLine(games.ToQueryString());
+    await games.ForEachAsync(g =>
+        Console.WriteLine($"{g.Name}, {g.Price} руб. [{g.CategoryId}]"));
+
+    Console.WriteLine();
+
+    var sqlTitle = new SqlParameter("@title", "SimCity");
+
+    games = context.Games
+        .FromSqlRaw($"select * from game where name = @title", sqlTitle);
+    Console.WriteLine(games.ToQueryString());
+    await games.ForEachAsync(g =>
+        Console.WriteLine($"{g.Name}, {g.Price} руб. [{g.CategoryId}]"));
+
+    Console.WriteLine();
+}
+
+static async Task SqlQuery(GamesDbContext context)
+{
+    var titles = context.Database
+        .SqlQuery<string>($"select name from game");
+    Console.WriteLine(titles.ToQueryString());
+    await titles.ForEachAsync(Console.WriteLine);
+
+    Console.WriteLine();
+
+    var minPrice = await context.Database
+        .SqlQuery<decimal>($"select min(price) as value from game")
+        .FirstOrDefaultAsync();
+    Console.WriteLine(minPrice);
+
+    //titles = context.Database.SqlQueryRaw();
+}
+
+static async Task ExecuteSql(GamesDbContext context)
+{
+    decimal addingPrice = -1m;
+
+    int changedRows = await context.Database
+        .ExecuteSqlAsync($"update game set price += {addingPrice}");
+    Console.WriteLine($"изменено {changedRows} строк");
+}
