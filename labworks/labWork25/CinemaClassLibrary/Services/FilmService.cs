aca56fb3302bec08b694b3b3bfd147c545222120
KlosHiHi <vaceslavvlasov420@gmail.com>
2025-11-27 23:27:12 +0300

    lw25v1

--- labworks/labWork25/CinemaClassLibrary/Services/FilmService.cs
+++ labworks/labWork25/CinemaClassLibrary/Services/FilmService.cs
@@ -2,32 +2,32 @@
 using CinemaDbLibrary.Contexts;
 using CinemaDbLibrary.Models;
 using Microsoft.EntityFrameworkCore;
-using System.Threading.Tasks;
 
 namespace CinemaClassLibrary.Services
 {
     public class FilmService(CinemaDbContext context)
     {
         private CinemaDbContext _context = context;
+        private string _baseItem = "---";
 
         public async Task<Film?> GetFilmByIdAsync(int id)
             => await _context.Films.FindAsync(id);
 
         public async Task<IEnumerable<FilmDto>> GetFilmsDtosAsync()
         {
-            using (CinemaDbContext db = new CinemaDbContext())
+            using (CinemaDbContext db = new())
             {
                 var filmsDtos = from f in db.Films
-                                     join s in db.Sessions on f.FilmId equals s.FilmId
-                                     join c in db.CinemaHalls on s.HallId equals c.HallId
-                                     select new FilmDto
-                                     {
-                                         Name = f.Name,
-                                         StartDate = s.StartDate,
-                                         CinemaName = c.CinemaName,
-                                         HallNumber = c.HallNumber,
-                                         Price = s.Price
-                                     };
+                                join s in db.Sessions on f.FilmId equals s.FilmId
+                                join c in db.CinemaHalls on s.HallId equals c.HallId
+                                select new FilmDto
+                                {
+                                    Name = f.Name,
+                                    StartDate = s.StartDate,
+                                    CinemaName = c.CinemaName,
+                                    HallNumber = c.HallNumber,
+                                    Price = s.Price
+                                };
 
                 filmsDtos = filmsDtos
                     .OrderBy(f => f.Name)
@@ -36,11 +36,27 @@ namespace CinemaClassLibrary.Services
             }
         }
 
-        public async Task<IEnumerable<FilmDto>> GetFilmDtoByDate(DateTime date)
+        public async Task<IEnumerable<string>> GetCinemaNamesAsync()
+        {
+            var names = await _context.CinemaHalls
+                        .GroupBy(h => h.CinemaName)
+                        .Select(h => h.Key)
+                        .ToListAsync();
+            names.Add(_baseItem);
+
+            return names;
+        }
+
+        public async Task<IEnumerable<FilmDto>> GetFilteredFilmDto(DateTime date, string cinemaName)
         {
             var filmsDtos = await GetFilmsDtosAsync();
 
-            return filmsDtos.Where(f => f.StartDate == date);
+            if(cinemaName != _baseItem)
+                filmsDtos = filmsDtos.Where(f => f.CinemaName == cinemaName);
+
+            filmsDtos = filmsDtos.Where(f => f.StartDate?.Date == date.Date );
+
+            return filmsDtos.ToList();
         }
     }
 }
---
+++ labworks/labWork25/LabWork25/FilmsWindows.xaml.cs
@@ -0,0 +1,81 @@
+﻿using CinemaClassLibrary.DTOs;
+using CinemaClassLibrary.Services;
+using CinemaDbLibrary.Contexts;
+using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+
+namespace LabWork25
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class FilmsWindows : Window
+    {
+        FilmService _service = new(new CinemaDbContext());
+
+        public FilmsWindows()
+        {
+            InitializeComponent();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await SetData();
+        }
+
+        private async Task SetData()
+        {
+            var filmDtos = await _service.GetFilmsDtosAsync();
+            var cinemaNames = await _service.GetCinemaNamesAsync();
+                       
+            CinemaNameComboBox.ItemsSource = cinemaNames;            
+            FilmDataGrid.ItemsSource = filmDtos;
+        }
+
+        private void ExportButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new()
+            {
+                Filter = "CSV files (*.csv)|*.csv|Text files (*.txt)|*.txt|All files (*.*)|*.*",
+                Title = "Сохранить таблицу"
+            };
+
+            if (saveFileDialog.ShowDialog() != true)
+                return;
+
+            var fileName = saveFileDialog.FileName;
+
+            ExportTableFiles(fileName);
+        }
+
+        private void ExportTableFiles(string fileName)
+        {
+            using (StreamWriter writer = new(fileName, false, System.Text.Encoding.Unicode))
+            {
+                writer.WriteLine("Название;Время начала сеанса;Кинотеатр;Номер зала;Цена");
+                var films = FilmDataGrid.Items.OfType<FilmDto>().ToList();
+
+                foreach (var film in films)
+                    writer.WriteLine($"{film.Name};{film.StartDate};{film.CinemaName};{film.HallNumber};{film.Price}");
+            }
+        }
+
+        private async void ShowFilteredFilmsButton_Click(object sender, RoutedEventArgs e)
+        {
+            var date = FilmsDatePicker.SelectedDate ?? DateTime.Now;
+            var cinemaName = CinemaNameComboBox.Text ?? "---";
+
+            var filteredFilms = await _service.GetFilteredFilmDto(date, cinemaName);
+
+            FilmDataGrid.ItemsSource = filteredFilms;
+        }
+
+        private async void ShowAllFilmsButton_Click(object sender, RoutedEventArgs e)
+        {
+            var filmDtos = await _service.GetFilmsDtosAsync();
+
+            FilmDataGrid.ItemsSource = filmDtos;
+        }
+    }
+}
