5efeb289308012d2fc70dcbb90df059218cf9dbd
DeDomosed <poperm137@gmail.com>
2025-12-11 12:01:07 +0300

    lw22

--- labworks/labwork22/task1.sql
+++ labworks/labwork22/task1.sql
@@ -1,7 +1,6 @@
-SELECT LastEnter,
-	CASE
+UPDATE Import1_User
+SET LastEnter	= CASE
 		WHEN CHARINDEX('/', Import1_User.LastEnter) > 0 THEN 
 		FORMAT(TRY_CONVERT(DATE, Import1_User.LastEnter, 101), 'dd.MM.yyyy') -- MM/dd/yyyy 
 		ELSE Import1_User.LastEnter
-		END AS Enter
-FROM Import1_User;
+		END;
